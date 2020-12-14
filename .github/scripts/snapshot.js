0985e8e565bb8e513c49d8f2349827a22ed6d004
Murph Murphy <murph@clurictec.com>
2020-12-14 13:26:20 -0700

    Last one, I promise

--- .github/scripts/snapshot.js
+++ .github/scripts/snapshot.js
@@ -45,7 +45,7 @@ if (lastCommitAuthor.stdout === "Leeroy Travis") {
 }
 
 //Ensure that our directory is set to the root of this project repo
-shell.cd(`${path.dirname(process.argv[1])}/../`);
+shell.cd(`${path.dirname(process.argv[1])}/../../`);
 
 //Run the build. This will cause the dist directory to be generated and also modify the package.json to bump it by a patch version
 shell.echo("\n\nRunning build process to generate dist output and bump version...\n");
